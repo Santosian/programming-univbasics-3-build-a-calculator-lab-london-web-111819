96ba887b8a4a4da9e2df2d87ed6c3b146e4e8d38
Santiago Molinari <santomolinari@gmail.com>
2019-11-07 16:03:52 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,44 @@
 def addition(num1, num2)
-
+  num3 = num1 + num2
+  puts num3
+  return num3
 end
 
 def subtraction(num1, num2)
-
+  num3 = num1 - num2
+  puts num3
+  return num3
 end
 
 def division(num1, num2)
-
+  num3 = num1 / num2
+  puts num3
+  return num3
 end
 
 def multiplication(num1, num2)
+  num3 = num1 * num2
+  puts num3
+  return num3
 
 end
 
 def modulo(num1, num2)
+    num3 = num1 % num2
+  puts num3
+  return num3
 
 end
 
 def square_root(num)
-
+  num3 = num**
+  puts num3
+  return num3
 end
+
+addition(5,4)
+subtraction(10,5)
+division(50,2)
+multiplication(4,30)
+modulo(34,5)
+square_root(9)
